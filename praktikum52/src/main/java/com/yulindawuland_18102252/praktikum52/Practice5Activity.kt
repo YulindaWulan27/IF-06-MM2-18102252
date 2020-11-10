7d38df96c44cd84f9cd8f677f2700bc45ea6889c
Yulinda wulan Dani <18102252@ittelkom-pwt.ac.id>
2020-11-10 05:38:53 -0800

    Initial commit

---
+++ praktikum52/src/main/java/com/yulindawuland_18102252/praktikum52/Practice5Activity.kt
@@ -0,0 +1,12 @@
+package com.yulindawuland_18102252.praktikum52
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Practice5Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practice5)
+    }
+}
