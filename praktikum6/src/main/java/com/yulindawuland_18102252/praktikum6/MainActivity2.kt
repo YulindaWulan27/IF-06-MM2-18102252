7bfd6e52e7fc9b135843fb7f66bd947894897e2d
Yulinda wulan Dani <18102252@ittelkom-pwt.ac.id>
2020-11-17 04:41:19 -0800

    praktikum6

---
+++ praktikum6/src/main/java/com/yulindawuland_18102252/praktikum6/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.yulindawuland_18102252.praktikum6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
