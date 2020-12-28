66a178816242360d0156a4ee9470358590f0ccde
Yulinda wulan Dani <18102252@ittelkom-pwt.ac.id>
2020-12-28 08:49:06 -0800

    create SettingPreferenceActivity

---
+++ Praktikum9/src/main/java/com/yulindawuland_18102252/praktikum9/SettingPreferenceActivity.kt
@@ -0,0 +1,11 @@
+package com.yulindawuland_18102252.praktikum9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_preference)
+    }
+}
