3e4db165236220b15e915b1e1bacdb9e18becd0b
Yulinda wulan Dani <18102252@ittelkom-pwt.ac.id>
2020-12-29 02:36:24 -0800

    Fininsh Praktikum9

--- Praktikum9/src/main/java/com/yulindawuland_18102252/praktikum9/MainActivity.kt
+++ Praktikum9/src/main/java/com/yulindawuland_18102252/praktikum9/MainActivity.kt
@@ -2,14 +2,56 @@ package com.yulindawuland_18102252.praktikum9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.yulindawuland_18102252.praktikum9.data.SettingModel
+import com.yulindawuland_18102252.praktikum9.preferences.SettingPreference
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        companion object {
+            private const val REQUEST_CODE = 100
+        }
+        private lateinit var settingModel: SettingModel
         private lateinit var binding: ActivityMainBinding
+        private lateinit var mSettingPreference: SettingPreference
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.act = this
         setContentView(binding.root)
         supportActionBar?.title = getString(R.string.main_title)
+        mSettingPreference = SettingPreference(this)
+        showExistingPreference()
     }
-}
+    private fun showExistingPreference() {
+        settingModel = mSettingPreference.getSetting()
+        populateView(settingModel)
+        fun openSetting(){
+            val intent = Intent(this@MainActivity, SettingPreferenceActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE)
+
+            override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+                super.onActivityResult(requestCode, resultCode, data)
+                if (requestCode == REQUEST_CODE) {
+                    if (resultCode == SettingPreferenceActivity.RESULT_CODE) {
+                        showExistingPreference()
+                    }
+                }
+            }
+        }
+
+    }
+
+    }
+    private fun populateView(settingModel: SettingModel) {
+        if (settingModel.isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            delegate.applyDayNight()
+        }
+        else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            delegate.applyDayNight()
+        }
+        binding.settingModel = settingModel
+    }
+
