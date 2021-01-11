12a69157f80efc174e64ec2b9d23182c61948949
Yulinda wulan Dani <18102252@ittelkom-pwt.ac.id>
2021-01-11 08:40:43 -0800

    SignUpActivity

---
+++ Praktikum11/src/main/java/com/yulindawuland_18102252/praktikum11/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.yulindawuland_18102252.praktikum11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
