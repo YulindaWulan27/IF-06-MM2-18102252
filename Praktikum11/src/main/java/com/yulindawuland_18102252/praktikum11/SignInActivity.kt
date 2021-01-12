70459aabdeef3e676962245f7ecc59fc32166613
Yulinda wulan Dani <18102252@ittelkom-pwt.ac.id>
2021-01-12 02:06:15 -0800

    praktikum 11

--- Praktikum11/src/main/java/com/yulindawuland_18102252/praktikum11/SignInActivity.kt
+++ Praktikum11/src/main/java/com/yulindawuland_18102252/praktikum11/SignInActivity.kt
@@ -1,11 +1,60 @@
 package com.yulindawuland_18102252.praktikum11
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnSign.setOnClickListener(this)
+        binding.tvSignUp.setOnClickListener(this)
+        binding.btnEmail.setOnClickListener(this)
+        binding.btnPhone.setOnClickListener(this)
+        auth = Firebase.auth
     }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnSign -> {
+                signIn(binding.inputEmail.text.toString(),
+                    binding.inputPassword.text.toString())
+            }
+            R.id.tvSignUp -> {
+                val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+    }
+
+    private fun signIn(email: String, password: String) {
+        if (!validateForm()) {
+            return
+        }
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    finish()
+                    val intent = Intent(this@SignInActivity,
+                        MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
+
 }
